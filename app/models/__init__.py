32c4178b164f8e18eeb1c2d0869a574584f599c1
KritimPrasadKafle <kritim10k@gmail.com>
2026-05-21 12:50:01 +0545

    feat: Changed the folder structure of the project

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.rider import Rider
-from app.models.driver import Driver
-from app.models.vehicle import Vehicle
-from app.models.trip import Trip
-from app.models.payment import Payment
